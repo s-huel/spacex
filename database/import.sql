e1864277929f9daecfd4535e9d25016138b9efba
Felix <felix.huel6@gmail.com>
2024-02-07 22:34:32 +0100

    database structure -created- and home changes

---
+++ database/import.sql
@@ -0,0 +1,15 @@
+ROP DATABASE IF EXISTS `spacex`;
+
+CREATE DATABASE `spacex`;
+
+USE `spacex`;
+
+
+
+CREATE TABLE `booking`
+
+CREATE TABLE `admins`
+
+CREATE TABLE `users`
+
+CREATE TABLE `flights`
--- index.php
+++ index.php
@@ -21,8 +21,38 @@
 
 <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
 <div id="main">
+    <h1 class="txt-main">The Future Is Here</h1>
 
+    <div class="video">
+        <iframe loading="lazy" width="1000" height="500" class="embed-responsive-item" src="https://www.youtube.com/embed/zqE-ultsWt0"
+            allowfullscreen></iframe>
+    </div>
+
+    <br>
+    <br>
+    <br>
+
+    <div class="grid-container">
+        <div class="card">
+        <div class="container">
+            <h4><b>Falcon Series</b></h4>
+            <p>
+            With our reusable Falcon 9 and Falcon Heavy rockets, we have successfully demonstrated the potential of reusable rocket technology for cost-effective space missions.
+            </p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="container">
+            <h4><b>Starbase</b></h4>
+            <p>
+              Starbase is a Starport where all our rockets are located. We repair, refill the fuel and check the rockets to make sure everyone has a safe trip to their destination.  
+            </p>
+        </div>
+    </div> 
 </div> 
+    </div>
+
+    
 
     <script src="javascript/main.js"></script>
 </body>
