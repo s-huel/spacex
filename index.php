6555d906ba62ef75282bc76688c273f49c9eb5e3
Felix <felix.huel6@gmail.com>
2024-02-29 10:17:43 +0100

    different home page design

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
   <a href="index.php">HOME</a>
-  <a href="#">BOOKING</a>
+  <a href="booking.php">BOOKING</a>
   <a href="#">INFO</a>
   <a href="#">ABOUT US</a>
 </div>
@@ -19,38 +19,56 @@
 <span onclick="openNav()"><img src="images/logo.png" alt="logo"></span>
 
 <div id="main">
-    <h1 class="txt-main">The Future Is Here</h1>
 
-    <div class="video">
+<section>
+  <h1 class="txt-main">The Future Is Here &#8595;</h1>
+</section>
+
+<section>
+  <div class="container reveal">
+    <div class="">
         <iframe width="1000" height="500" class="embed-responsive-item" src="https://www.youtube.com/embed/zqE-ultsWt0"
             allowfullscreen></iframe>
     </div>
+  </div>
+</section>
 
-    <br>
-    <br>
-    <br>
-
-    <div class="grid-container">
+<section>
+  <div class="container reveal">
+    <h2>Info</h2>
+    <div class="text-container">
+      <div class="text-box">
         <div class="card">
-        <div class="container">
-            <h4><b>Falcon Series</b></h4>
-            <p>
-            With our reusable Falcon 9 and Falcon Heavy rockets, we have successfully demonstrated the potential of reusable rocket technology for cost-effective space missions.
-            </p>
+            <div class="container">
+                <h3>Falcon 9</h3>
+                <p>
+                    With our reusable Falcon 9 and Falcon Heavy rockets, we have successfully demonstrated the potential of reusable rocket technology for cost-effective space missions.
+                    <br>
+                    <br>
+                    <a href="#" class="cardlink">READ MORE >></a>
+                </p>
+            </div>
         </div>
+      </div>
+      <div class="text-box">
+        <div class="card">
+            <div class="container">
+                <h3>Starbase</h3>
+                <p>
+                  Starbase is a Starport where all our rockets are located. We repair, refill the fuel and check the rockets to make sure everyone has a safe trip to their destination.
+                  <br>
+                  <br>
+                  <a href="#" class="cardlink">READ MORE >></a> 
+                </p>
+            </div>
+        </div>
+      </div>
     </div>
-    <div class="card">
-        <div class="container">
-            <h4><b>Starbase</b></h4>
-            <p>
-              Starbase is a Starport where all our rockets are located. We repair, refill the fuel and check the rockets to make sure everyone has a safe trip to their destination.  
-            </p>
-        </div>
-    </div> 
+  </div>
+</section>
 </div> 
 
-    
+<script src="javascript/main.js"></script>
 
-    <script src="javascript/main.js"></script>
 </body>
 </html>
--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,29 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-  } 
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+} 
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
