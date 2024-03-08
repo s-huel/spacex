417f77f7c5e38ad04e461fa2aaf9e1fb63ccf369
Felix <felix.huel6@gmail.com>
2024-03-08 10:10:24 +0100

    restart

--- index.php
+++ index.php
@@ -2,75 +2,13 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Starport</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home - Starport</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-
-<img src="images/logo.png" alt="logo">
-<p>&copy; 2024 f-huel. All rights reserved.</p>
-
-<input type="checkbox" id="active">
-    <label for="active" class="menu-btn"><span></span></label>
-    <label for="active" class="close"></label>
-    <div class="wrapper">
-      <ul>
-        <li><a href="index.php">HOME</a></li>
-        <li><a href="booking.php">BOOKING</a></li>
-        <li><a href="#">INFO</a></li>
-        <li><a href="#">CONTACT</a></li>
-      </ul>
-    </div>
-
-    <div class="content">
-        <div class="title">
-          <section>
-            <h1>The Future Is Here &#8595;</h1></section>
-          </div>
-            
-      <section>
-        <div class="container reveal">
-          <iframe width="1000" height="500" class="embed-responsive-item" src="https://www.youtube.com/embed/zqE-ultsWt0"
-          allowfullscreen></iframe>
-        </div>
-      </section>
-
-      <section>
-        <div class="container reveal fade-bottom">
-          <h2>INFO</h2>
-          <div class="text-container">
-            <div class="text-box">
-              <h3>Falcon 9</h3>
-              <p>
-                With our reusable Falcon 9 and Falcon Heavy rockets, we have successfully demonstrated the potential of reusable rocket technology for cost-effective space missions.
-              </p>
-              <br>
-              <a href="#" class="link">READ MORE >></a>
-            </div>
-            <div class="text-box">
-              <h3>Starports</h3>
-              <p>
-                Starbase is a Starport where all our rockets are located. We repair, refill the fuel and check the rockets to make sure everyone has a safe trip to their destination.
-              </p>
-              <br>
-              <a href="#" class="link">READ MORE >></a>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-      
-</input>
-
-
-
-<script src="javascript/main.js"></script>
 
 </body>
 </html>
