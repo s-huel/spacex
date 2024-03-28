0a355e8b3d6d501e58b03b3845c08fedd73f2a62
Felix <felix.huel6@gmail.com>
2024-03-28 13:33:30 +0100

    contact page Added google maps, struggled with making the map responsive

--- contact.php
+++ contact.php
@@ -50,23 +50,22 @@ if (isset($_GET['logout'])) {
             <div class="row">
                 <article>
                     <h3>Email</h3>
-                    <p>info@spacex.com</p>
+                    <p><a href="mailto:info@spacex.com">info@spacex.com</a></p>
                 </article>
                 <article>
                     <h3>Phone</h3>
-                    <p>+1-310-363-6000</p>
+                    <p><a href="tel:+1-310-363-6000">+1-310-363-6000</a></p>
                 </article>
             </div>
         </section>
 
         <section class="reveal">
-            <h1>Location</h1>
            <div class="row"> 
                 <article>
-                    <iframe src=
-                        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6780516.420455391!2d-127.29404520159186!3d33.920571528675076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b5dee46db32d%3A0x5589bf4232c10232!2sSpacex!5e0!3m2!1sen!2snl!4v1711531511475!5m2!1sen!2snl" width="300" height="300" style="border:0;"
-                        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-                    </iframe>
+                    <h3>Location</h3>
+                    <div class="map-container">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6780516.420455391!2d-127.29404520159186!3d33.920571528675076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b5dee46db32d%3A0x5589bf4232c10232!2sSpacex!5e0!3m2!1sen!2snl!4v1711531511475!5m2!1sen!2snl" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </article>
                 <article>
                     <h3>Visit Us Instead?</h3>
--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var animatedElements = document.querySelectorAll('.reveal');
-    var windowHeight = window.innerHeight;
-  
-    function revealElements() {
-      animatedElements.forEach(function(element) {
-        var elementTop = element.getBoundingClientRect().top;
-  
-        if (elementTop < windowHeight) {
-          element.classList.add('visible');
-        }
-      });
-    }
-    revealElements();
+document.addEventListener("DOMContentLoaded", function () {
+  var animatedElements = document.querySelectorAll('.reveal');
+  var windowHeight = window.innerHeight;
 
-    window.addEventListener('scroll', function() {
-      revealElements();
+  function revealElements() {
+    animatedElements.forEach(function (element) {
+      var elementTop = element.getBoundingClientRect().top;
+
+      if (elementTop < windowHeight) {
+        element.classList.add('visible');
+      }
     });
+  }
+  revealElements();
+
+  window.addEventListener('scroll', function () {
+    revealElements();
   });
-  
+
+  window.addEventListener('resize', function () {
+    var sidenav = document.querySelector('.sidenav');
+    var content = document.querySelector('.content');
+    if (window.innerWidth < 900) {
+      sidenav.style.display = 'none';
+      content.style.marginLeft = '0';
+    } else {
+      sidenav.style.display = 'block';
+      content.style.marginLeft = '15%';
+    }
+  });
+});
